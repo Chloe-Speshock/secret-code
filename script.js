bbb2a568098ded4694e070226bde2e48cf937db5
Chloe Speshock <cespeshock@gmail.com>
2024-01-30 11:22:06 -0600

    created javascript alert

---
+++ script.js
@@ -0,0 +1,12 @@
+// declare variables for num1 num2 and num3
+// create an alert for the page that shows the secret combination
+
+//these are the variables
+let num1 = 5 * 2;
+let num2 = 30 + 10;
+let num3 = 40 - 1;
+
+//Here is the alert
+alert(
+  `You are recieving this message because you have been chosen to open an important vault.  The combination is ${num1} - ${num2} - ${num3}`
+);
